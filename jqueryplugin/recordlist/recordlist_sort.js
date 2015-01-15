30955a2de8ba9802a2b26170e1c9a0bc5cadcda7
thzt <lvlufeng@126.com>
2015-01-15 15:16:05 +0800

    fix bug: sort table will lose tr attr info.

--- jqueryplugin/recordlist/recordlist_sort.js
+++ jqueryplugin/recordlist/recordlist_sort.js
@@ -1,110 +1,110 @@
-(function($,_){
-	$.pluginManager.extend('recordList',{
-		sort:sort
-	});
-	
-	function sort(){
-		var container=this.eq(0),
-			afterSort=arguments[0]&&arguments[0].afterSort;
-		
-		container.find('>table>thead>tr>td').each(function(){
-			var td=$(this),
-				canSortThisColumn=td.attr('data-sort')!=null;
-				
-			if(!canSortThisColumn){
-				return true;
-			}
-			
-			td.addClass('thzt_recordlist_sort_init');
-		});
-			
-		container.delegate('>table>thead>tr>td','click',function(e){
-			e.stopPropagation();
-			
-			var td=$(this),
-				columnIndex=td.index(),
-				canSortThisColumn=td.attr('data-sort')!=null;
-				
-			if(!canSortThisColumn){
-				return;
-			}
-				
-			if(!td.hasClass('thzt_recordlist_sort_ascend')){
-				sortColumn.call(container,{
-					columnIndex:columnIndex
-				});
-				
-				td.removeClass()
-					.addClass('thzt_recordlist_sort_ascend')
-					.siblings('td')
-					.filter(function(){
-						var td=$(this),
-							canSortThisColumn=td.attr('data-sort')!=null;
-						
-						return canSortThisColumn;
-					})
-					.removeClass()
-					.addClass('thzt_recordlist_sort_init');
-				
-				afterSort&&afterSort();
-				return;
-			}
-			
-			sortColumn.call(container,{
-				columnIndex:columnIndex,
-				descend:true
-			});
-			
-			td.removeClass()
-				.addClass('thzt_recordlist_sort_descend')
-				.siblings('td')
-				.filter(function(){
-					var td=$(this),
-						canSortThisColumn=td.attr('data-sort')!=null;
-					
-					return canSortThisColumn;
-				})
-				.removeClass()
-				.addClass('thzt_recordlist_sort_init');
-				
-			afterSort&&afterSort();
-		});
-		
-		return this;
-	}
-	
-	function sortColumn(){
-		var container=this.eq(0),
-			columnIndex=arguments[0].columnIndex,
-			descend=arguments[0].descend,
-			
-			notEmptyTrs=container.find('>table>tbody>tr').filter(function(){
-				var tr=$(this),
-					td=tr.find('>td').eq(columnIndex),
-					key=td.attr('data-key');
-					
-				return key!=null;
-			}),
-			sortedTrs=_.sortBy(notEmptyTrs, function(v){
-				var tr=$(v),
-					value=tr.find('>td').eq(columnIndex).html().trim();
-					
-				return value;
-			}),
-			sortedTbodyHtml=(descend?_.reduceRight:_.reduce)
-				(sortedTrs,function(m,v){
-					var tr=$(v),
-						trHtml=tr.html();
-				
-					return m+'<tr>'+trHtml+'</tr>';
-				},''),
-				
-			pageSize=container.recordList('getPageSize');
-				
-		container.recordList('clearDataList');
-		container.find('>table>tbody').html(sortedTbodyHtml);
-		container.recordList('page',pageSize);
-		
-		return this;
-	}
-}(jQuery,_));
+(function ($, _) {
+    $.pluginManager.extend('recordList', {
+        sort: sort
+    });
+
+    function sort() {
+        var container = this.eq(0),
+            afterSort = arguments[0] && arguments[0].afterSort;
+
+        container.find('>table>thead>tr>td').each(function () {
+            var td = $(this),
+                canSortThisColumn = td.attr('data-sort') != null;
+
+            if (!canSortThisColumn) {
+                return true;
+            }
+
+            td.addClass('lilly_recordlist_sort_init');
+        });
+
+        container.delegate('>table>thead>tr>td', 'click', function (e) {
+            e.stopPropagation();
+
+            var td = $(this),
+                columnIndex = td.index(),
+                canSortThisColumn = td.attr('data-sort') != null;
+
+            if (!canSortThisColumn) {
+                return;
+            }
+
+            if (!td.hasClass('lilly_recordlist_sort_ascend')) {
+                sortColumn.call(container, {
+                    columnIndex: columnIndex
+                });
+
+                td.removeClass()
+                    .addClass('lilly_recordlist_sort_ascend')
+                    .siblings('td')
+                    .filter(function () {
+                        var td = $(this),
+                            canSortThisColumn = td.attr('data-sort') != null;
+
+                        return canSortThisColumn;
+                    })
+                    .removeClass()
+                    .addClass('lilly_recordlist_sort_init');
+
+                afterSort && afterSort();
+                return;
+            }
+
+            sortColumn.call(container, {
+                columnIndex: columnIndex,
+                descend: true
+            });
+
+            td.removeClass()
+                .addClass('lilly_recordlist_sort_descend')
+                .siblings('td')
+                .filter(function () {
+                    var td = $(this),
+                        canSortThisColumn = td.attr('data-sort') != null;
+
+                    return canSortThisColumn;
+                })
+                .removeClass()
+                .addClass('lilly_recordlist_sort_init');
+
+            afterSort && afterSort();
+        });
+
+        return this;
+    }
+
+    function sortColumn() {
+        var container = this.eq(0),
+            columnIndex = arguments[0].columnIndex,
+            descend = arguments[0].descend,
+
+            notEmptyTrs = container.find('>table>tbody>tr').filter(function () {
+                var tr = $(this),
+                    td = tr.find('>td').eq(columnIndex),
+                    key = td.attr('data-key');
+
+                return key != null;
+            }),
+            sortedTrs = _.sortBy(notEmptyTrs, function (v) {
+                var tr = $(v),
+                    value = tr.find('>td').eq(columnIndex).html().trim();
+
+                return value;
+            }),
+            sortedTbodyHtml = (descend ? _.reduceRight : _.reduce)
+                (sortedTrs, function (m, v) {
+                    var tr = $(v),
+                        trHtml = tr[0].outerHTML;
+
+                    return m + trHtml;
+                }, ''),
+
+            pageSize = container.recordList('getPageSize');
+
+        container.recordList('clearDataList');
+        container.find('>table>tbody').html(sortedTbodyHtml);
+        container.recordList('page', pageSize);
+
+        return this;
+    }
+} (jQuery, _));
