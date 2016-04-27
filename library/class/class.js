6ce05493929ab691de3cedde84d6bf84c1f79cd1
thzt <thzt.xx@gmail.com>
2016-04-27 21:43:46 +0800

    update

--- library/class/class.js
+++ library/class/class.js
@@ -4,15 +4,12 @@
 		extend:extend
 	};
 
-	function extend(p){
-		var Sub=function(){},
-			Super=this;
-
-		Sub.prototype=connectPrototype(p,Super.prototype);
-		Sub.prototype.constructor=Sub;
+	function extend(proto){
+		var Super=this,
+			protoChainHead=connectPrototype(proto,Super.prototype),
+			Sub=createConstructor(protoChainHead);
 
 		Sub.extend=extend;
-
 		return Sub;
 	}
 
@@ -20,12 +17,18 @@
 		if(p2==null){
 			return p1;
 		}
-		
-		var T=function(){};
-		T.prototype=p2;
-		T.prototype.constructor=T;
 
-		return Object.assign(new T,p1);
-    };
+		var C=createConstructor(p2);
+		return Object.assign(new C,p1);
+    }
+
+	function createConstructor(proto){
+		var C=function(){};
+		
+		C.prototype=proto;
+		C.prototype.constructor=C;
+
+		return C;
+	}
 
 }(window));
