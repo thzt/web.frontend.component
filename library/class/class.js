e8c2d01613056821edfca7d645790694b649d2c9
thzt <lvlufeng@126.com>
2016-04-27 22:06:39 +0800

    update

--- library/class/class.js
+++ library/class/class.js
@@ -6,21 +6,16 @@
 
 	function extend(proto){
 		var Super=this,
-			protoChainHead=connectPrototype(proto,Super.prototype),
-			Sub=createConstructor(protoChainHead);
+			Sub;
 
+		Super.prototype!=null
+			&&(proto=Object.assign(Object.create(Super.prototype),proto));
+		
+		Sub=createConstructor(proto);
 		Sub.extend=extend;
+		
 		return Sub;
 	}
-
-	function connectPrototype(p1,p2){
-		if(p2==null){
-			return p1;
-		}
-
-		var C=createConstructor(p2);
-		return Object.assign(new C,p1);
-    }
 
 	function createConstructor(proto){
 		var C=function(){};
