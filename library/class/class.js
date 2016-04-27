88e4d089059864bb77b3b5f6e09b4c2d9e78878e
thzt <lvlufeng@126.com>
2016-04-27 20:48:52 +0800

    update

--- library/class/class.js
+++ library/class/class.js
@@ -1,30 +1,26 @@
 (function(global){
 
 	global.Class={
-		create:function(p1){
-			var Base=function(){};
-
-			Base.prototype=p1;
-			Base.prototype.constructor=Base;
-
-			Base.extend=function(p2){
-				var Sub=function(){},
-					Super=this;
-
-				var prototype=connectPrototype(p2,Super.prototype);
-				Sub.prototype=prototype;
-				Sub.prototype.constructor=Sub;
-
-				Sub.extend=Super.extend;
-
-				return Sub;
-			};
-
-			return Base;
-		}
+		extend:extend
 	};
 
+	function extend(p){
+		var Sub=function(){},
+			Super=this;
+
+		Sub.prototype=connectPrototype(p,Super.prototype);
+		Sub.prototype.constructor=Sub;
+
+		Sub.extend=extend;
+
+		return Sub;
+	}
+
 	function connectPrototype(p1,p2){
+		if(p2==null){
+			return p1;
+		}
+		
 		var T=function(){};
 		T.prototype=p2;
 		T.prototype.constructor=T;
