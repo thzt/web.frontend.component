9e0a77a9fbac2e783e70ff6fd1297f568551f3f8
thzt <lvlufeng@126.com>
2016-06-06 14:14:31 +0800

    update

--- library/preventevent/preventevent.js
+++ library/preventevent/preventevent.js
@@ -1,29 +1,32 @@
-(function(global){
+(function (global) {
 
-	global.preventEvent=preventEvent;
+	global.preventEvent = preventEvent;
 
-	function preventEvent(opt){
-		var lock=opt.lock,
-			release=opt.release,
-			event=opt.event,
+	function preventEvent(opt) {
+		var lock = opt.lock,
+			release = opt.release,
+			event = opt.event,
 
-			isLocked=false;
+			isLocked = false;
 
-		return function(){
-			var context=this;
+		return function () {
+			var context = this,
+				args = arguments;
 
-			if(isLocked){
+			if (isLocked) {
 				return;
 			}
-			
-			lock.call(context);
-			isLocked=true;
-			
-			event.call(context,function(){
-				release.call(context);
-				isLocked=false;
+
+			lock.apply(context, args);
+			isLocked = true;
+
+			var eventArgs = [].slice.call(args, 0);
+			eventArgs.unshift(function () {
+				release.apply(context, args);
+				isLocked = false;
 			});
+			event.apply(context, eventArgs);
 		};
 	}
-	
-}(window));
+
+} (window));
