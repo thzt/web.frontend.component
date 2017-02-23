e59874fcaf20e89c88532d073b31869aef352539
thzt <lvlufeng@126.com>
2017-02-23 17:34:53 +0800

    add chainContinuation

---
+++ library/chain-continuation/index.js
@@ -0,0 +1,49 @@
+const chainContinuation = (...funcs) => ({params, shouldBreak}, cont) => recursiveCore(funcs, params, shouldBreak, [], cont);
+
+const recursiveCore = (funcs, params, shouldBreak, accumulation, cont) => {
+    if (funcs.length == 0) {
+        cont(...accumulation);
+        return;
+    }
+
+    const func = funcs.shift();
+    const param = params.shift();
+
+    func(param, (...args) => {
+        accumulation.push(args);
+
+        if (shouldBreak(...args)) {
+            cont(...accumulation);
+            return;
+        }
+
+        recursiveCore(funcs, params, shouldBreak, accumulation, cont);
+    });
+};
+
+export default chainContinuation;
+
+// const f1 = (option, cont) => {
+//     console.log(option);    //1
+//     cont(true, 123);
+// }
+
+// const f2 = (option, cont) => {
+//     console.log(option);    //2
+//     cont(false, 456);
+// };
+
+// const f3 = (option, cont) => {
+//     console.log(option);
+//     cont(true, 789);
+// };
+
+// const f = chainContinuation(f1, f2, f3);
+// f({
+//     params: [1, 2, 3],
+//     shouldBreak: (isPass, message) => {
+//         return !isPass;
+//     }
+// }, (...args) => {
+//     console.log(JSON.stringify(args));    //[[true,123],[false,456]]
+// });
