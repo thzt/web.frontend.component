2550f07ffb655e66d9a2b954abae1782e307603a
thzt <thzt.xx@gmail.com>
2017-10-17 13:40:52 +0800

    update

--- library/yield-reduce/src/reduce.js
+++ library/yield-reduce/src/reduce.js
@@ -1,15 +1,13 @@
 // use case 
 // yield* reduce(arr, function*(memo,val,index){ }, init)
-const reduce = function (array, gen, init) {
-    return (function* () {
-        let memo = init;
-        for (let i = 0; i < array.length; i++) {
-            const val = array[i];
-            memo = yield* gen.call(this, memo, val, i);
-        }
+const reduce = function* (array, gen, init) {
+    let memo = init;
+    for (let i = 0; i < array.length; i++) {
+        const val = array[i];
+        memo = yield* gen.call(this, memo, val, i);
+    }
 
-        return memo;
-    }).call(this);
+    return memo;
 };
 
 export default reduce;
